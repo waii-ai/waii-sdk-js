1cdf03c036a002fb5fea4211d5693734d26c40ee
Gunther Hagleitner <g@waii.ai>
2024-02-07 20:40:50 -0800

    update to include new SemanticContext

--- clients/semantic-context/src/SemanticContext.ts
+++ clients/semantic-context/src/SemanticContext.ts
@@ -9,12 +9,25 @@ class SemanticStatement {
     scope: string
     statement: string
     labels?: string[]
+    always_include?: boolean
+    search_keys?: string[]
+    extract_prompt?: string
 
-    public constructor(scope: string = '*', statement: string, labels: string[] = []) {
+    public constructor(
+        scope: string = '*',
+        statement: string,
+        labels: string[] = [],
+        always_include: boolean = true,
+        search_keys: string[] = [],
+        extract_prompt: string = ''
+    ) {
         this.id = uuidv4();
         this.scope = scope;
         this.statement = statement;
         this.labels = labels;
+        this.always_include = always_include;
+        this.search_keys = search_keys;
+        this.extract_prompt = extract_prompt;
     }
 }
 
@@ -28,11 +41,20 @@ type ModifySemanticContextResponse = {
     deleted?: string[]
 }
 
+type GetSemanticContextRequestFilter = {
+    always_include?: boolean
+}
+
 type GetSemanticContextRequest = {
+    filter?: GetSemanticContextRequestFilter
+    search_text?: string
+    offset?: number
+    limit?: number
 }
 
 type GetSemanticContextResponse = {
     semantic_context?: SemanticStatement[]
+    total_candidates?: number
 }
 
 export let SemanticContext = (
@@ -67,6 +89,7 @@ export {
     SemanticStatement,
     ModifySemanticContextRequest,
     ModifySemanticContextResponse,
+    GetSemanticContextRequestFilter,
     GetSemanticContextRequest,
     GetSemanticContextResponse
 };
