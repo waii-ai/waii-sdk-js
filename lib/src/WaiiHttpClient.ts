a24aada4917d1281f9a9173d6f6ceea88a82db3f
Wangda Tan <wangda@apache.org>
2024-03-20 11:42:04 -0700

    Make fetch function can be configured so waii-cli can use undici library

--- lib/src/WaiiHttpClient.ts
+++ lib/src/WaiiHttpClient.ts
@@ -6,10 +6,12 @@ class WaiiHttpClient {
     scope: string = '';
     orgId: string = '';
     userId: string = '';
+    fetch_func: any;
 
-    public constructor(url: string = 'http://localhost:9859/api/', apiKey: string = '') {
+    public constructor(url: string = 'http://localhost:9859/api/', apiKey: string = '', fetch_func = fetch) {
         this.url = url;
         this.apiKey = apiKey;
+        this.fetch_func = fetch_func;
     };
 
     public setScope(scope: string) {
@@ -51,7 +53,7 @@ class WaiiHttpClient {
         let timer;
         let fetchOrTimeout = Promise.race(
             [
-                fetch(this.url + endpoint, request),
+                this.fetch_func(this.url + endpoint, request),
                 new Promise<Response>(
                     (res, rej) => {timer = setTimeout(
                         () => rej(new Error(`Call timed out after ${this.timeout} ms`)),
